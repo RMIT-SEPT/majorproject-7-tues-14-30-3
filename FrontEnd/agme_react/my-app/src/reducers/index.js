be2684dc3deb1ce76b5829f12c48487c79aed15e
s3784244 <s3784244@student.rmit.edu.au>
2020-10-18 23:22:01 +1100

    Fixed compilation error

--- FrontEnd/agme_react/my-app/src/reducers/index.js
+++ FrontEnd/agme_react/my-app/src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers, createStore } from "redux";
 import errorReducer from "./errorReducer";
 import loggedInReducer from './loggedInReducer';
-import currentReducer from './currentUser';
 import messageReducer from './errorMessage';
 import accountReducer from "./accountReducer";
 import securityReducer from "./securityReducer";
@@ -12,7 +11,6 @@ import timesReducer from "./timesReducer";
 const rootReducer = combineReducers({
   errors: errorReducer,
   loggedIn: loggedInReducer,
-  currentUser:currentReducer,
   message:messageReducer,
   account:accountReducer,
   security:securityReducer,
